b444c99db424d8970f641154b0eb326bafa0c7e2
Parrot-zzt <13822593+parrot-zzt@user.noreply.gitee.com>
2025-03-22 23:17:46 +0000

    feat: add ApiStack with POST /things and api key protection

--- bin/ds-assignment1.ts
+++ bin/ds-assignment1.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { DatabaseStack } from '../lib/database-stack';
 import { LambdaStack } from '../lib/lambda-stack';
+import { ApiStack } from '../lib/api-stack';
 
 const app = new cdk.App();
 
@@ -9,6 +10,9 @@ const app = new cdk.App();
 const databaseStack = new DatabaseStack(app, 'DatabaseStack');
 
 // Step 2: Create the Lambda stack and pass the DynamoDB table to it
-new LambdaStack(app, 'LambdaStack', {
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
   table: databaseStack.table,
+});
+new ApiStack(app, 'ApiStack', {
+  postLambda: lambdaStack.postLambda,
 });
---
+++ lib/api-stack.ts
@@ -0,0 +1,55 @@
+// lib/api-stack.ts
+
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { LambdaIntegration, RestApi, UsagePlan, ApiKey, MethodLoggingLevel } from 'aws-cdk-lib/aws-apigateway';
+import { Function } from 'aws-cdk-lib/aws-lambda';
+
+interface ApiStackProps extends StackProps {
+  postLambda: Function;
+}
+
+export class ApiStack extends Stack {
+  public readonly apiKey: ApiKey;
+
+  constructor(scope: Construct, id: string, props: ApiStackProps) {
+    super(scope, id, props);
+
+    // Create a new REST API
+    const api = new RestApi(this, 'ThingsApi', {
+      restApiName: 'Things Service',
+      deployOptions: {
+        loggingLevel: MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+      },
+    });
+
+    // Add a resource: /things
+    const thingsResource = api.root.addResource('things');
+
+    // Create Lambda integration for POST
+    const postIntegration = new LambdaIntegration(props.postLambda);
+
+    // Create an API key
+    this.apiKey = new ApiKey(this, 'ThingsApiKey');
+
+    // Create usage plan
+    const plan = new UsagePlan(this, 'UsagePlan', {
+      name: 'BasicUsagePlan',
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+
+    // Associate the key with the plan
+    plan.addApiKey(this.apiKey);
+
+    // Add POST method with API key required
+    thingsResource.addMethod('POST', postIntegration, {
+      apiKeyRequired: true,
+    });
+  }
+}
